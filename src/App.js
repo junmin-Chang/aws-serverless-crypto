70004f225ed799e32f004e3bf726ec02933cd8bd
junmin-chang <chjm219@kookmin.ac.kr>
2021-07-20 13:49:30 +0900

    api

--- src/App.js
+++ src/App.js
@@ -4,25 +4,35 @@ import { API } from "aws-amplify";
 
 function App() {
     const [coins, updateCoins] = useState([])
+    const [input, updateInput] = useState({ limit: 5, start: 0})
 
-    // API 호출을 위한  fetchCoins 함수 정의
-
-    async function fetchCoins() {
-        const data = await API.get('cryptoapi', '/coins')
-        updateCoins(data.coins)
-        console.log(data)
+    function updateInputValues(type, value) {
+        updateInput({ ...input, [type] : value})
     }
-    useEffect(() => {
-        fetchCoins()
-    },[])
+    // API 호출을 위한  fetchCoins 함수 정의
+    async function fetchCoins() {
+        const { limit, start } = input
+        const data = await API.get('cryptoapi', `/coins?start=${start}&limit=${limit}`)
+        updateCoins(data.coins)
+    }
+
+
     return (
             <div className="App">
+                <input
+                    onChange={e => updateInputValues('limit', e.target.value) }
+                    placeholder="limit"
+                />
+                <input
+                    onChange={e => updateInputValues('start', e.target.value)}
+                    placeholder="start"
+                />
+                <button onClick={fetchCoins}>Fetch Coins</button>
               {
                   coins.map((coin, index) => (
                       <div key={index}>
                           <h2>{coin.name} - {coin.symbol}</h2>
                           <h5>${coin.price_usd}</h5>
-
                       </div>
                   ))
               }
